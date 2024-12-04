8fb3495a10fe71f6026cc4cb47dd67f69951f4d9
Rafael Gadotti <rafael.gadotti01@gmail.com>
2024-12-03 22:03:29 -0300

    Autenticação Facebook

---
+++ fblogin/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:fblogin/facebooklogin/loginWithFacebook.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginWithFacebook(),
+    );
+  }
+}
